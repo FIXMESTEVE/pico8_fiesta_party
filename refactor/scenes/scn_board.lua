4abae3d2ee8a7cff527f36fa4a1a59bbb0f2020d
s4e-tom <tom.solacroup@gmail.com>
2019-11-27 14:01:20 +0100

    started working on the linker cell tool

--- refactor/scenes/scn_board.lua
+++ refactor/scenes/scn_board.lua
@@ -3,15 +3,18 @@ scn_board._init=function()
     debug=false
     -- boardstate="begin"
 	boardstate="editor"
+
+	--editor mode vars
 	editor_cells_menu={}
 	editor_cells={}
+	linker_cell=nil
 	local start_cell={type=1,letter="s",col=5,selected=false,x1=0,x2=0,y1=0,y2=0} --start
 	local path_select_cell={type=2,letter="p",col=5,selected=false,x1=0,x2=0,y1=0,y2=0} --path select
 	local blue_cell={type=3,letter="b",col=12,selected=false,x1=0,x2=0,y1=0,y2=0} --blue cell
 	local red_cell={type=4,letter="r",col=8,selected=false,x1=0,x2=0,y1=0,y2=0} --red cell
+	local dummy_cell={type=5,letter="",col=5,selected=false,x1=0,x2=0,y1=0,y2=0} --dummy cell
 	local eraser={type=-1,letter="x",col=5,selected=false,x1=0,x2=0,y1=0,y2=0} --eraser tool
 	local linker={type=0,letter="|",col=5,selected=false,x1=0,x2=0,y1=0,y2=0} --linker tool
-
 	add(editor_cells_menu,start_cell)
 	add(editor_cells_menu,path_select_cell)
 	add(editor_cells_menu,blue_cell)
@@ -19,12 +22,14 @@ scn_board._init=function()
 	add(editor_cells_menu,eraser)
 	add(editor_cells_menu,linker)
 
+	--camera
     xcam=0
     ycam=0
 	
     player_start_x=224
     player_start_y=336
 
+	--players
     players={}
     p1=player:new(1,player_start_x,player_start_y,128,0,0,"blue")
     p2=player:new(2,player_start_x+16,player_start_y,144,0,0,"red")
@@ -69,8 +74,8 @@ scn_board._init=function()
 	
 	if(boardstate=="editor")then
 		poke(0x5f2d,1)
-		mousex=stat(32)
-		mousey=stat(33)
+		mousex=stat(32)+xcam
+		mousey=stat(33)+ycam
 		mouseb=stat(34)
 	end
 end
@@ -97,6 +102,10 @@ function draw_editor()
 	for i=1,#editor_cells_menu do
 		if(editor_cells_menu[i].selected==true)then
 			if(editor_cells_menu[i].type>0)drawcell(mousex-mousex%8,mousey-mousey%8,(mousex-mousex%8)+7,(mousey-mousey%8)+7,editor_cells_menu[i].letter,editor_cells_menu[i].col,false)
+		
+			if(linker_cell!=nil and editor_cells_menu[i].type==0)then
+				line(linker_cell.x1+(linker_cell.x2-linker_cell.x1)/2,linker_cell.y1+(linker_cell.y2-linker_cell.y1)/2,mousex,mousey,7)
+			end
 		end
 		drawcell(editor_cells_menu[i].x1,editor_cells_menu[i].y1,editor_cells_menu[i].x2,editor_cells_menu[i].y2,editor_cells_menu[i].letter,editor_cells_menu[i].col,editor_cells_menu[i].selected)
 	end
@@ -155,7 +164,7 @@ function update_editor()
 				if(editor_cells_menu[i].type==-1)then
 					erase_cell_under_cursor(mousex,mousey)
 				elseif(editor_cells_menu[i].type==0)then
-					--todo: linker tool
+					link_cell_under_cursor(mousex,mousey)
 				else
 					--todo: replace this with a copy constructor
 					--todo: prevent cell placement on already existing cell (or erase existing cell)
@@ -177,15 +186,33 @@ function update_editor()
 	end
 end
 
+function link_cell_under_cursor(mousex,mousey)
+	local c=find_cell(x,y)
+	if(c==nil)return
+	if(linker_cell==nil)then
+		linker_cell=c
+	else
+		--todo
+	end
+end
+
 function erase_cell_under_cursor(mousex,mousey)
-	--todo: eraser tool
+	local c=find_cell(x,y)
+	if(c!=nil)then
+		del(editor_cells,c)
+		if(c==linker_cell)linker_cell=nil
+	end
+end
+
+function find_cell(x,y)
 	local x=mousex-mousex%8
 	local y=mousey-mousey%8
 	for c in all(editor_cells) do
 		if(c.x1==x and c.y1==y)then
-			del(editor_cells,c)
+			return c
 		end
 	end
+	return nil
 end
 
 function draw_map()
